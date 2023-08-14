245d40195d0fed0d120e9792867d220308793bcd
Vineet <singhvineet.y17@gmail.com>
2023-08-14 14:53:35 -0400

    p3 b exercise

--- p3_exer/index.js
+++ p3_exer/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 var morgan=require("morgan");
+const cors=require("cors");
+
+
 
 app.use(express.json());
+app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 let persons = [
@@ -88,6 +92,7 @@ app.post("/api/persons",(req,res)=>{
 
 
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`App running on ${PORT}`);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
